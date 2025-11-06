ab233e990520d6e77f8f205c7d270dab587d1db7
Carlos Villar Cejalvo <cvilcej@upv.edu.es>
2025-11-06 09:03:21 +0100

    Añadida interfaz genérica List

--- List.h
+++ List.h
@@ -7,7 +7,15 @@ class List {
     public:
       virtual ~List() {}
       
-      virtual void insert(int pos, T e) = 0;  // ... aquí los métodos virtuales puros
+      virtual void insert(int pos, T e) = 0;  // ... aquí los métodos virtual
+      virtual void append(T e) = 0;
+      virtual void prepend(T e) = 0;
+      virtual T remove(int pos) = 0;
+      virtual const T get(int pos) = 0;
+      virtual const int search(T e) = 0;
+      virtual const bool empty() = 0;
+      virtual const int size() = 0;
 };
 
 #endif
+
